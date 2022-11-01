af823f0c3bda9f2dfdcedc5cfcd51d83367d9458
Daniel <daniel@Zipcoders-MacBook-Pro-10.local>
2022-11-01 13:54:32 -0400

    haven't really solved anything yet

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,25 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
+    Scanner input = new Scanner(System.in);
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        System.out.println("input a number please");
+        Scanner input = new Scanner(System.in);
+        int n = Integer.parseInt(input.nextLine());
+
+        for (int i = 1; i < n; i++) {
+            n += i;
+        }
+        int answer = n;
+
 
     }
-    
     int sumOfNumbers(int n) {
+
         return 0;
     }
     
